3400af562d40b399d96704ba1e58d554639d4549
whyy <icarovsilva1@gmail.com>
2023-01-07 16:20:28 -0300

    feat: create new transaction toast on success and on error  transaction declined

--- src/pages/Trade/index.tsx
+++ src/pages/Trade/index.tsx
@@ -1,58 +1,12 @@
-import { CaretRight, CurrencyEth, UserSwitch } from 'phosphor-react'
-import { useId } from 'react'
-import { useForm } from 'react-hook-form'
+import { CaretRight } from 'phosphor-react'
 import { Link } from 'react-router-dom'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
 import { useTranslation } from 'react-i18next'
 
-import { walletService } from '../../services/metamask.service'
-import { Button } from '../../components/Button'
-
-import { Column, Container, ContainerDescription, Form, Row } from './styles'
-import { useWallets } from '../../store/wallet'
-
-const schema = z.object({
-  to: z.string(),
-  amount: z.number().positive(),
-  description: z.string().optional(),
-})
-
-type IFormProps = z.infer<typeof schema>
+import { Container } from './styles'
+import { Form } from './Form'
 
 export function Trade() {
-  const toWalletId = useId()
   const { t } = useTranslation()
-  const { wallets, setWalletValue } = useWallets((state) => state)
-  const { register, handleSubmit, watch, reset } = useForm<IFormProps>({
-    defaultValues: {
-      description: '',
-      to: '',
-    },
-    resolver: zodResolver(schema),
-  })
-
-  const submitIsNotAvailable = !watch('amount') && !watch('to')
-
-  async function onSubmit(props: IFormProps) {
-    try {
-      const { amount, to } = props
-      if (!wallets) {
-        return
-      }
-      const response = await walletService.createTransaction({
-        amount,
-        to,
-        from: wallets[0],
-      })
-      const newBalance = await walletService.getWalletsBalance(wallets[0])
-      setWalletValue(newBalance)
-      console.log(response)
-      reset()
-    } catch (err) {
-      console.log(err)
-    }
-  }
 
   return (
     <Container>
@@ -67,55 +21,7 @@ export function Trade() {
           <li>{t('Links.trader')}</li>
         </ul>
       </header>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Column>
-          <label htmlFor={toWalletId}>
-            {t('Inputs.Labels.walletAddress')} :
-          </label>
-          <Row>
-            <UserSwitch size={32} />
-            <input
-              placeholder={
-                t('Inputs.Placeholders.placeholderAddress') as string
-              }
-              id={toWalletId}
-              {...register('to')}
-            />
-          </Row>
-        </Column>
-        <Column>
-          <label htmlFor={toWalletId}>
-            {t('Inputs.Labels.amountTransferred')} :
-          </label>
-          <Row>
-            <CurrencyEth size={32} />
-            <input
-              type="number"
-              placeholder={
-                t('Inputs.Placeholders.amountTransferredPlaceholder') as string
-              }
-              id={toWalletId}
-              {...register('amount', { valueAsNumber: true })}
-            />
-          </Row>
-        </Column>
-        <Column>
-          <label htmlFor={toWalletId}>
-            {t('Inputs.Labels.transactionTransaction')} :
-          </label>
-          <ContainerDescription>
-            <textarea
-              cols={100}
-              rows={10}
-              {...register('description')}
-              disabled
-            />
-          </ContainerDescription>
-        </Column>
-        <footer>
-          <Button disabled={submitIsNotAvailable}>{t('send')}</Button>
-        </footer>
-      </Form>
+      <Form />
     </Container>
   )
 }
