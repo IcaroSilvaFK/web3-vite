4b240beb9f90d8a67918ed554a92746c9a91f6d7
whyy <icarovsilva1@gmail.com>
2023-01-09 20:07:50 -0300

    feat: create home styles

--- apps/bets/src/pages/index.tsx
+++ apps/bets/src/pages/index.tsx
@@ -1,3 +1,18 @@
+import { Head } from '@title'
+
+import { Container, Header } from 'styles/pages/home.styles'
+
 export default function Home() {
-  return <div>Hello world</div>
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Container>
+        <Header>
+          <h1>Pools</h1>
+        </Header>
+      </Container>
+    </>
+  )
 }
